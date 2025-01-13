f7939e9e5965b2b4cc077a59fc1e6a54f072b7e3
MoSwilam <muswilam@gmail.com>
2025-01-13 14:46:18 +0100

    add images for  auth and notifications

--- apps/notifications/src/main.ts
+++ apps/notifications/src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const globalPrefix = 'api/notifications';
+    app.setGlobalPrefix(globalPrefix);
+    const port = process.env.PORT || 3001;
+    await app.listen(port);
+    Logger.log(
+      `🚀 Notifications app is running on: http://localhost:${port}/${globalPrefix}`,
+    );
 }
 bootstrap();
